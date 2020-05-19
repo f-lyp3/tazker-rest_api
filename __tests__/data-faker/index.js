b22edaee9b17a4e0a9b229fc2d4537f6cc806361
f-lyp3 <filipy7lype@gmail.com>
2020-05-19 12:08:48 +0100

    import normalize functions and use function data fakers

--- __tests__/data-faker/index.js
+++ __tests__/data-faker/index.js
@@ -1,9 +1,14 @@
 const faker = require("faker");
 
+const {
+    capitalizeText,
+    capitalizeWord
+} = require("../../src/utils/normalizer");
+
 function generateFakeUser(overrides){
     return Object.freeze({
-        firstName: faker.name.firstName(),
-        lastName: faker.name.firstName(),
+        firstName: capitalizeWord(faker.name.firstName()),
+        lastName: capitalizeWord(faker.name.firstName()),
         email: faker.internet.email(),
         password: "M1p4$$0rD",
         ...overrides // Manual fields and overrides
@@ -12,7 +17,7 @@ function generateFakeUser(overrides){
 
 function generateFakeTask(overrides){
     return Object.freeze({
-        name: faker.lorem.words(3),
+        name: capitalizeText(faker.lorem.words(3)),
         ownerId: "5e8454326b9a0b352e98a9a4",
         parentId: null,
         ...overrides
