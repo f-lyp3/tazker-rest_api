7548bcd2b731603f32bbea3b0112bbb2ec09ae5b
f-lyp3 <filipy7lype@gmail.com>
2020-05-19 11:44:34 +0100

    add noSensitive function to and exports all functions

--- src/utils/normalizer.js
+++ src/utils/normalizer.js
@@ -1,3 +1,20 @@
 function capitalize(word){
     return word[0].toUpperCase() + word.slice(1)
 }
+
+function noSensitive(obj, sensitives){
+    const newObj = {};
+
+    for(let property of Object.keys(obj)){
+        if(!sensitives.includes(property)){
+            newObj[property] = obj[property];
+        }
+    }
+
+   return newObj
+}
+
+module.exports = Object.freeze({
+    capitalize,
+    noSensitive
+});
