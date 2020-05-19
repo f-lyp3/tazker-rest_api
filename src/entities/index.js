d0a20d2a9ff6f1dec73feeedbf95b4f69ea67f13
f-lyp3 <filipy7lype@gmail.com>
2020-05-19 11:48:28 +0100

    import capitalize function from src/utils/normalizer

--- src/entities/index.js
+++ src/entities/index.js
@@ -1,16 +1,23 @@
 const crypto = require("crypto");
 
 const { mustHaveError } = require("../utils/errors");
+const { capitalize } = require("../utils/normalizer");
+
 const {
     isValidEmail,
     isValidID,
     isValidName,
-    isFinePassword } = require("../utils/validators");
+    isFinePassword
+} = require("../utils/validators");
 
 const buildMakeUser = require("./user");
 const buildMakeTask = require("./task");
 
-const makeUser = buildMakeUser({ mustHaveError, isValidEmail, isFinePassword, isValidName });
+const makeUser = buildMakeUser({
+    mustHaveError, isValidEmail,
+    isFinePassword, isValidName, capitalize
+});
+
 const makeTask = buildMakeTask({mustHaveError, isValidID, makeHash})
 
 module.exports = Object.freeze({
