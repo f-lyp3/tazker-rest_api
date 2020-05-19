a2d269171ae54cd413ae90a0e478738d608bd5d7
f-lyp3 <filipy7lype@gmail.com>
2020-05-19 15:46:00 +0100

    import noSensitive function from utils/normalizer and pass down to all methods in user services

--- src/services/user/index.js
+++ src/services/user/index.js
@@ -2,6 +2,7 @@ const { UserDb } = require("../../database");
 const { hashPassword } = require("../auth/passwords");
 
 const { isValidID, isValidEmail } = require("../../utils/validators");
+const { noSensitive } = require("../../utils/normalizer");
 
 const buildAddUser = require("./add-user");
 const buildFindUserByEmail = require("./find-user-byEmail");
@@ -10,11 +11,15 @@ const buildUpdateUser = require("./update-user");
 const buildRemoveUser = require("./remove-user");
 
 
-const addUser = buildAddUser({ UserDb, hashPassword });
-const updateUser = buildUpdateUser({ UserDb, isValidID, hashPassword });
-const removeUser = buildRemoveUser({ UserDb, isValidID });
-const findUserByEmail = buildFindUserByEmail({ UserDb, isValidEmail });
-const findUser = buldFindUser({ UserDb, isValidID });
+const addUser = buildAddUser({ UserDb, hashPassword, noSensitive });
+const updateUser = buildUpdateUser({
+    UserDb, isValidID, hashPassword, noSensitive 
+});
+const removeUser = buildRemoveUser({ UserDb, isValidID, noSensitive });
+const findUserByEmail = buildFindUserByEmail({
+    UserDb, isValidEmail, noSensitive
+});
+const findUser = buldFindUser({ UserDb, isValidID, noSensitive });
 
 module.exports = Object.freeze({
     addUser,
